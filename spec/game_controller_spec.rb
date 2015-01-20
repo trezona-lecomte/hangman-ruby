38fe209f3bfcb59d428a48f4f2db7804fa822303
trezona-lecomte <trezona.lecomte@gmail.com>
2015-01-20 22:53:33 +1300

    Add tests for game_over? method in GameController.

--- spec/game_controller_spec.rb
+++ spec/game_controller_spec.rb
@@ -13,5 +13,25 @@ describe GameController do
       end
     end
   end
-  
+
+  describe '#game_over?' do
+    context 'when the game is won' do
+      before do
+        allow(controller.game).to receive(:won).and_return true
+      end
+      it 'displays a congratulatory message' do
+        expect(controller.view).to receive(:congratulate)
+        controller.game_over?
+      end
+    end
+    context 'when the game is lost' do
+      before do
+        allow(controller.game).to receive(:lost).and_return true
+      end
+      it 'displays a commiseratory message' do
+        expect(controller.view).to receive(:commiserate)
+        controller.game_over?
+      end
+    end
+  end
 end
