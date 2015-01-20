9517b06509c0ffcf0529145f0f587fae92e5986f
trezona-lecomte <trezona.lecomte@gmail.com>
2015-01-20 22:52:03 +1300

    Change MAX_NUM_STARTING_PIECES to 10

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,20 +1,18 @@
 class Hangman
-  
   attr_reader :current_pieces
   
   STARTING_NUM_OF_PIECES = 0
-  MAX_NUM_OF_PIECES = 27
+  MAX_NUM_OF_PIECES = 10
   
 	def initialize
     @current_pieces = STARTING_NUM_OF_PIECES
 	end
   
   def add_piece
-    @current_pieces = current_pieces + 1
+    @current_pieces += 1
   end
   
   def hanged?
     current_pieces == MAX_NUM_OF_PIECES ? true : false
-  end
-  
+  end  
 end
