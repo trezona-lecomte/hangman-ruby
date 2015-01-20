9e38282d553184c5b1767c768a8e7ee4bd52925f
trezona-lecomte <trezona.lecomte@gmail.com>
2015-01-20 20:26:45 +1300

    Replace 'while not game_over?' with 'until game_over?' in GameController, remove unnecessary whitespace, rename start_game to run_game.

--- lib/game_controller.rb
+++ lib/game_controller.rb
@@ -2,26 +2,25 @@ require_relative 'console_ui'
 require_relative 'game'
 
 class GameController
-
   attr_reader :game
   attr_reader :view
 
-	def initialize
-	end
+  def initialize
+  end
 
-  def start_game
+  def run_game
     @game = Game.new
     @view = ConsoleUI.new
     @view.welcome
-    while not game_over?
+    until game_over?
       main_game_loop
     end
   end
 
   def main_game_loop
-    @view.show_obfuscated_word( game.word )
+    @view.show_obfuscated_word(game.word)
     @view.prompt_for_guess
-    @game.guess( @view.capture_guess )
+    @game.guess(@view.capture_guess)
   end
 
   def game_over?
